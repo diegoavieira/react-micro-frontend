810d0cd558206851aebf7f8235843fcbc59cc70e
Diego Adilson Vieira <diegoavieira@gmail.com>
2021-07-07 23:42:39 -0300

    build: fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import { createTheme, ThemeProvider, Button } from '@material-ui/core';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#006400"
+    },
+    secondary: {
+      main: "#ffa500"
+    }
+  }
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+    </ThemeProvider>
   );
 }
 
