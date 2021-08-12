38c21696c505d3629f0986309dada85d3abe7cce
Diego Adilson Vieira <diegoavieira@gmail.com>
2021-08-12 16:58:55 -0300

    adjustments

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,13 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const validation = (validate, value) => {
-  switch (validate.type) {
+  const rule = validate && typeof validate === 'string' ? { check: validate } : validate;
+
+  [rule.check, rule.value = null] = rule.check.split('=');
+
+  switch (rule.check) {
     case 'required':
-      return { valid: !!value, message: validate.message || 'Required!' };
+      return { check: rule.check, valid: !!value, message: rule.message || 'Required!' };
     case 'minLength':
       return {
-        valid: !!(value && validate.value <= value.length),
-        message: validate.message || `Min length is ${validate.value}.`
+        check: rule.check,
+        valid: !!(value && +rule.value <= value.length),
+        message: rule.message || `Min length is ${rule.value}.`
+      };
+    case 'email':
+      return {
+        check: rule.check,
+        valid: /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(value),
+        message: rule.message || 'Invalid format.'
       };
     default:
       return { valid: true };
@@ -15,43 +26,85 @@ const validation = (validate, value) => {
 };
 
 const useForm = (fields) => {
-  const initValues = Object.keys(fields).reduce((prev, key) => ({ ...prev, [key]: fields[key].value }), {});
+  const fieldsKeys = Object.keys(fields);
+  const initValues = fieldsKeys.reduce((prev, key) => ({ ...prev, [key]: fields[key][0] }), {});
+  const initErrors = fieldsKeys.reduce((prev, key) => ({ ...prev, [key]: { invalid: false } }), {});
+
   const [values, setValues] = useState(initValues);
-  const [errors, setErros] = useState(initValues);
+  const [errors, setErrors] = useState(initErrors);
+  const [invalid, setInvalid] = useState(true);
+  const [submitted, setSubmitted] = useState(false);
 
-  const setFieldErrors = (name, value) => {
-    if (fields[name].validates && fields[name].validates.length) {
-      const fieldErrors = { [name]: { invalid: false, message: [] } };
+  const fieldErrors = (name, value) => {
+    const validates = fields[name][1];
 
-      fields[name].validates.forEach((validate) => {
+    if (validates && validates.length) {
+      validates.forEach((validate) => {
         const checkValid = validation(validate, value);
 
         if (!checkValid.valid) {
-          fieldErrors[name].invalid = true;
-          fieldErrors[name].message = [...fieldErrors[name].message, checkValid.message];
+          errors[name] = { ...errors[name], invalid: true, [checkValid.check]: checkValid.message };
         }
       });
-
-      [fieldErrors[name].message] = fieldErrors[name].message;
-
-      setErros({ ...errors, ...fieldErrors });
     }
+
+    return errors;
   };
+
+  useEffect(() => {
+    Object.keys(values).forEach((key) => {
+      const validates = fields[key][1];
+
+      setInvalid(!!(validates && validates.length));
+
+      if (values[key]) {
+        setErrors({ ...fieldErrors(key, values[key]) });
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    setSubmitted(false);
+  }, [submitted]);
 
   const onChange = (event) => {
     const { name, value } = event.target;
-
     setValues({ ...values, [name]: value });
-    setFieldErrors(name, value);
+    setErrors({ ...errors, [name]: initErrors[name] });
+    console.log(errors);
   };
 
   const onBlur = (event) => {
     const { name, value } = event.target;
-
-    setFieldErrors(name, value);
+    console.log(fieldErrors(name, value));
   };
 
-  return [onChange, onBlur, values, errors];
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // Object.keys(values).forEach((key) => setFieldErrors(key, values[key]));
+    setSubmitted(true);
+  };
+
+  const onReset = () => {
+    setValues(initValues);
+    setErrors(initErrors);
+    setSubmitted(false);
+  };
+
+  useEffect(() => {
+    // Object.keys(values).forEach((key) => {
+    //   console.log(key);
+    //   const validates = fields[key][1];
+    //   setInvalid(!!(validates && validates.length));
+    //   // if (values[key]) {
+    //   //   setFieldErrors(key, values[key]);
+    //   // }
+    // });
+    // const hasError = Object.keys(errors).filter((key) => errors[key].invalid);
+    // setInvalid(!!hasError.length);
+  }, [invalid]);
+
+  return { values, errors, invalid, submitted, onChange, onBlur, onSubmit, onReset };
 };
 
 export default useForm;
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,44 +1,68 @@
-import React from 'react';
-import { makeStyles, TextField } from '@material-ui/core';
+import React, { useEffect } from 'react';
+import { Button, makeStyles, TextField } from '@material-ui/core';
 import withToast from '../hocs/withToast';
 import useForm from '../hooks/useForm';
 
 const useStyles = makeStyles(() => ({
   create: {
     padding: '24px'
+  },
+  fieldHeight: {
+    height: '78px',
+    marginBottom: '4px'
   }
 }));
 
 const fields = {
-  name: {
-    value: 'asd',
-    validates: [
-      { type: 'required', message: 'Field is required.' },
-      { type: 'minLength', value: 5 }
-    ]
-  }
+  name: ['asd', ['required', 'minLength=5']],
+  email: ['', ['required', { check: 'email', message: 'Email invalid' }]]
 };
 
 const Create = () => {
   const classes = useStyles();
-  const [onChange, onBlur, values, errors] = useForm(fields);
+  const form = useForm(fields);
+
+  useEffect(() => {
+    if (form.submitted) {
+      console.log('valuess', form.values);
+      // form.onReset();
+    }
+  }, [form]);
 
   return (
     <div className={classes.create}>
-      <form noValidate autoComplete="off">
+      <form noValidate autoComplete="off" onSubmit={form.onSubmit}>
         <TextField
+          className={classes.fieldHeight}
           variant="filled"
           fullWidth
           type="text"
           label="Name"
           name="name"
-          onChange={onChange}
-          onBlur={onBlur}
-          value={values.name}
+          onChange={form.onChange}
+          onBlur={form.onBlur}
+          value={form.values.name}
           required
-          error={errors.name.invalid}
-          helperText={errors.name.message}
+          error={form.errors.name.invalid}
+          helperText={form.errors.name.required || form.errors.name.minLength}
         />
+        <TextField
+          className={classes.fieldHeight}
+          variant="filled"
+          fullWidth
+          type="email"
+          label="Email"
+          name="email"
+          onChange={form.onChange}
+          onBlur={form.onBlur}
+          value={form.values.email}
+          required
+          error={form.errors.email.invalid}
+          helperText={form.errors.email.required || form.errors.email.email}
+        />
+        <Button disabled={form.invalid} type="submit">
+          Submit
+        </Button>
       </form>
     </div>
   );
